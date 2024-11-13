c54b609a7a6c4564f11643097b10511c8d31d245
Nathan Wandji <nwandji@Nathans-MacBook-Pro.local>
2024-11-12 22:04:35 -0500

    First commit

---
+++ bwCollector/main.go
@@ -0,0 +1,7 @@
+package main
+
+// In this file I will write the code to create
+// - Read a yaml configuration to find the logs folders with the regex to scan
+// - Scan the directionaries ( I will actually try to explore different ways of doing that)
+// - Start a thread to read the logs files
+// - Implement a strategy to make sure I dont read what has already been read.
