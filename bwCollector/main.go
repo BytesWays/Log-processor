3ed7e7daa55cb341303a166910ba5d3e9881e508
Nathan Wandji <nwandji@Nathans-MacBook-Pro.local>
2024-11-14 00:03:10 -0500

    Learned about: Defer, pointer, os.ReadFile, os.OpenFile, panic

--- bwCollector/main.go
+++ bwCollector/main.go
@@ -5,3 +5,42 @@ package main
 // - Scan the directionaries ( I will actually try to explore different ways of doing that)
 // - Start a thread to read the logs files
 // - Implement a strategy to make sure I dont read what has already been read.
+
+// Day 4: Today I am working getting data from the config file.
+// the config file should have data on which files to take. So I will learn how to read a file and parse the information
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type logsPath struct {
+	LogPath []string `yaml:"log"`
+}
+
+func (l *logsPath) LoadConfig(configFile string) {
+	log.Println("Loading configuration file")
+	file, err := os.ReadFile(configFile)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = yaml.Unmarshal(file, &l)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(*&l.LogPath)
+
+}
+
+func main() {
+
+	var l logsPath
+	l.LoadConfig("config.yaml")
+
+}
